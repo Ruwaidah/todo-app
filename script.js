a20c547b0b82845f5b6e4d840fca46f6bfce4f55
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-05-20 13:16:30 -0500

    adding local storage to save todo list after refresh

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ console.log(todoForm);
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 const ulDiv = document.querySelector(".unorder-list-div");
+const savedTodo = JSON.parse(localStorage.getItem("todo")) || [];
+
+for (let i = 0; i < savedTodo.length; i++) {
+  const li = document.createElement("li");
+  const para = document.createElement("p");
+  const btn = document.createElement("button");
+  btn.innerText = "Delete";
+  para.innerText = savedTodo[i].todo;
+  li.append(para);
+  li.append(btn);
+  ul.append(li);
+  if (savedTodo[i].isDone) para.classList.toggle("done");
+}
 
 todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value.length > 0) {
-    if (document.querySelector("p")) document.querySelector("p").remove();
+    if (document.querySelector("#errorMsg"))
+      document.querySelector("#errorMsg").remove();
     const li = document.createElement("li");
-    li.innerText = input.value;
+    const para = document.createElement("p");
+    para.innerText = input.value;
     const btn = document.createElement("button");
     btn.innerText = "Delete";
+    li.append(para);
     li.append(btn);
     ul.append(li);
+    savedTodo.push({ todo: input.value, isDone: false });
+    localStorage.setItem("todo", JSON.stringify(savedTodo));
     input.value = "";
   } else {
-    if (!document.querySelector("p")) {
+    if (!document.querySelector("#errorMsg")) {
       const errorMsg = document.createElement("p");
+      errorMsg.setAttribute("id", "errorMsg");
       errorMsg.innerText = "Can't add empty field";
       ulDiv.prepend(errorMsg);
     }
@@ -25,14 +44,22 @@ todoForm.addEventListener("submit", function (e) {
 });
 
 ul.addEventListener("click", function (e) {
-    console.log(e.target.nodeName )
-  if (e.target.nodeName === "LI") {
-    e.target.classList.toggle("done");
+  const eleme = e.target;
+  if (e.target.nodeName === "P") {
+    const toggle = e.target.classList.toggle("done");
+    for (let element of savedTodo) {
+      if (element.todo === eleme.innerText) {
+        element.isDone = !element.isDone;
+      }
+    }
+  } else if (e.target.nodeName === "BUTTON") {
+    for (let i = 0; i < savedTodo.length; i++) {
+      if (savedTodo[i].todo === e.target.previousSibling.innerText) {
+        console.log(i);
+        savedTodo.splice(i, 1);
+      }
+    }
+    e.target.parentElement.remove();
   }
-  else if ( e.target.nodeName === "BUTTON") {
-    console.log(e.target.parentElement)
-    e.target.parentElement.remove()
-  }
+  localStorage.setItem("todo", JSON.stringify(savedTodo));
 });
-
-
